34fd92d50f4424da0c75f2fd33a9422459a53ccd
Eric Kidd <git@randomhacks.net>
2015-11-07 06:26:20 -0500

    Add log2

--- crates/alloc_toyos/src/math.rs
+++ crates/alloc_toyos/src/math.rs
@@ -4,7 +4,7 @@ use std::num::Wrapping;
 pub trait PowersOf2 {
     fn is_power_of_2(self) -> bool;
     fn next_power_of_2(self) -> usize;
-    //fn log2(self) -> u8;
+    fn log2(self) -> u8;
 }
 
 impl PowersOf2 for usize {
@@ -41,6 +41,24 @@ impl PowersOf2 for usize {
         assert!(result >= self && self > result >> 1);
         result
     }
+
+    /// Calculate the base-2 logarithm of this value.
+    ///
+    /// This will normally round down, except for the case of `0.log2()`,
+    /// which will return 0.
+    ///
+    /// Based on the obvious code at
+    /// http://graphics.stanford.edu/~seander/bithacks.html#IntegerLogObvious
+    fn log2(self) -> u8 {
+        let mut temp = self;
+        let mut result = 0;
+        temp >>= 1;
+        while temp != 0 {
+            result += 1;
+            temp >>= 1;
+        }
+        result
+    }
 }
 
 #[test]
@@ -73,4 +91,13 @@ fn test_next_power_of_2() {
     assert_eq!(32, 17.next_power_of_2());
     assert_eq!(32, 32.next_power_of_2());
     assert_eq!(8388608, 8376263.next_power_of_2());
+}
+
+#[test]
+fn test_log2() {
+    assert_eq!(0, 0.log2());
+    assert_eq!(0, 1.log2());
+    assert_eq!(1, 2.log2());
+    assert_eq!(5, 32.log2());
+    assert_eq!(10, 1024.log2());
 }
