f2b4981f9e184846816dbbad294c69624a137d9d
Eric Kidd <git@randomhacks.net>
2015-11-08 19:47:42 -0500

    cpuio: Remove redundant comments

--- crates/cpuio/src/x86.rs
+++ crates/cpuio/src/x86.rs
@@ -2,7 +2,8 @@
 
 /// Read a `u8`-sized value from `port`.
 pub unsafe fn inb(port: u16) -> u8 {
-    // The registers for this instruction are always the same.
+    // The registers for the `in` and `out` instructions are always the
+    // same: `a` for value, and `d` for the port address.
     let result: u8;
     asm!("inb %dx, %al" : "={al}"(result) : "{dx}"(port) :: "volatile");
     result
@@ -10,13 +11,11 @@ pub unsafe fn inb(port: u16) -> u8 {
 
 /// Write a `u8`-sized `value` to `port`.
 pub unsafe fn outb(port: u16, value: u8) {
-    // The registers for this instruction are always the same.
     asm!("outb %al, %dx" :: "{dx}"(port), "{al}"(value) :: "volatile");
 }
 
 /// Read a `u16`-sized value from `port`.
 pub unsafe fn inw(port: u16) -> u16 {
-    // The registers for this instruction are always the same.
     let result: u16;
     asm!("inw %dx, %ax" : "={ax}"(result) : "{dx}"(port) :: "volatile");
     result
@@ -24,13 +23,11 @@ pub unsafe fn inw(port: u16) -> u16 {
 
 /// Write a `u8`-sized `value` to `port`.
 pub unsafe fn outw(port: u16, value: u16) {
-    // The registers for this instruction are always the same.
     asm!("outw %ax, %dx" :: "{dx}"(port), "{ax}"(value) :: "volatile");
 }
 
 /// Read a `u32`-sized value from `port`.
 pub unsafe fn inl(port: u16) -> u32 {
-    // The registers for this instruction are always the same.
     let result: u32;
     asm!("inl %dx, %eax" : "={eax}"(result) : "{dx}"(port) :: "volatile");
     result
@@ -38,6 +35,5 @@ pub unsafe fn inl(port: u16) -> u32 {
 
 /// Write a `u32`-sized `value` to `port`.
 pub unsafe fn outl(port: u16, value: u32) {
-    // The registers for this instruction are always the same.
     asm!("outl %eax, %dx" :: "{dx}"(port), "{eax}"(value) :: "volatile");
 }
