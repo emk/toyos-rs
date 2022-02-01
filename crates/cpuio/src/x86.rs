7e6215d9019ea6adfa5744de92ed9ca888c8acf6
ondra05 <ondra@ondra05.cz>
2022-02-01 23:18:25 +0100

    CPU IO fix

--- crates/cpuio/src/x86.rs
+++ crates/cpuio/src/x86.rs
@@ -1,39 +1,38 @@
 //! Rust wrappers around the x86-family I/O instructions.
 
-/// Read a `u8`-sized value from `port`.
+use core::arch::asm;
+/// Read an `u8`-sized value from `port`.
 pub unsafe fn inb(port: u16) -> u8 {
-    // The registers for the `in` and `out` instructions are always the
-    // same: `a` for value, and `d` for the port address.
-    let result: u8;
-    llvm_asm!("inb %dx, %al" : "={al}"(result) : "{dx}"(port) :: "volatile");
-    result
+    let out;
+    asm!("in al, dx", out("al") out, in("dx") port);
+    out
 }
 
-/// Write a `u8`-sized `value` to `port`.
+/// Write an `u8`-sized `value` to `port`.
 pub unsafe fn outb(value: u8, port: u16) {
-    llvm_asm!("outb %al, %dx" :: "{dx}"(port), "{al}"(value) :: "volatile");
+    asm!("out dx, al", in("dx") port, in("al") value);
 }
 
-/// Read a `u16`-sized value from `port`.
+/// Read an `u16`-sized value from `port`.
 pub unsafe fn inw(port: u16) -> u16 {
-    let result: u16;
-    llvm_asm!("inw %dx, %ax" : "={ax}"(result) : "{dx}"(port) :: "volatile");
-    result
+    let out;
+    asm!("in ax, dx", out("ax") out, in("dx") port);
+    out
 }
 
-/// Write a `u8`-sized `value` to `port`.
+/// Write an `u16`-sized `value` to `port`.
 pub unsafe fn outw(value: u16, port: u16) {
-    llvm_asm!("outw %ax, %dx" :: "{dx}"(port), "{ax}"(value) :: "volatile");
+    asm!("out dx, ax", in("dx") port, in("ax") value);
 }
 
-/// Read a `u32`-sized value from `port`.
+/// Read an `u32`-sized `value` from `port`.
 pub unsafe fn inl(port: u16) -> u32 {
-    let result: u32;
-    llvm_asm!("inl %dx, %eax" : "={eax}"(result) : "{dx}"(port) :: "volatile");
-    result
+    let out;
+    asm!("in eax, dx", out("eax") out, in("dx") port);
+    out
 }
 
-/// Write a `u32`-sized `value` to `port`.
+/// Write an `u32`-sized `value` to `port`.
 pub unsafe fn outl(value: u32, port: u16) {
-    llvm_asm!("outl %eax, %dx" :: "{dx}"(port), "{eax}"(value) :: "volatile");
+    asm!("out dx, eax", in("dx") port, in("eax") value);
 }
